034be2c3c2371b6422a819601f04a17ed4d66a88
williamslaur <williamslaur@yahoo.com>
2018-04-29 19:12:41 -0500

    all items

--- HTMLForm/scripts/main.js
+++ HTMLForm/scripts/main.js
@@ -1,78 +1,90 @@
 $(function(){
     
     $('#btnSubmit').on('click', submit);
-         
-        
-function submit() {
-    var elm;
-    var errorList = [];
-    var inputs = ['firstName', 'lastName', 'emailAddress', 'textarea', ];
-    var ERROR_CLASS = 'invalid';
-    debugger
+    $('#btnReset').on('click', reset);
 
-    for (var i = 0; i < inputs.length; i++) {
-        elm = getE(inputs[i]);
-        if (elm.value === '') {
-            errorList.push(elm.placeholder + ' is required');
+    var inputs = ['firstName', 'lastName', 'emailAddress'];
+
+    var ERROR_CLASS = 'invalid';
+
+    function submit(){
+        var errorList = [];
+        var elm;
+       //finds the text input values and sets error message and changes class if empty
+        for (var i=0; i<inputs.length; i++) {
+            elm = getE(inputs[i]);
+            if (elm.val === "") {
             elm.classList.add(ERROR_CLASS);
+            errorList.push(elm.placeholder + "is required.");
         } else {
             elm.classList.remove(ERROR_CLASS);
         }
-    }
-
-    var email = getE('emailAddress');
-    if (email.value.indexOf('@') == -1) {
-        errorList.push('Enter a valid Email');
-    }
-
-    var email2 = getE('emailAddress');
-    if (email.value.indexOf('.com') == "") {
-        errorList.push('Email must contain ".com" in the address');
-    }
-
+    //console.log(errorList)
+        //check for email address to have .com 
+        if ($('#emailAddress').indexOf('.com') = false) {
+            errorList.push('Email address must include .com.');
+        }
     
-    var terms = getE('terms');
-    if (terms.checked === false) {
+        //find the terms input values and sets error message and changes class if empty
+        var terms = getE('terms');
+        if (terms.checked === false) {
+            errorList.push('You must accept the terms and conditions');
+        }
+        //find the value for findUs and return error if empty
+        var howFind = getE('findUs');
+        if (howFind.val === "") {
+            errorList.push('You must select an option for how you found us.');
+        }
+        //find the value for textarea and return error if empty
+        if ($('#textarea').val = "" ) {
+            errorList.push('You must complete the reason for Why you need this blender.');
+        };
 
-        errorList.push('You must accept the terms and conditions');
-    }
-
-
-    var html
+    //console.log(errorList)
+        // enters the errorList into the messages and if none- returns thanks 
+        var html
     if (errorList.length > 0) {
         html = '<ul class="errors"><li>' + errorList.join('</li><li>') + '</li></ul>';
     } else {
         html = '<h1>Thank you for registering</h1>';
-        reset();
+     //   reset();
     }
-
+    //sets the new messages value to be displayed at the top of page
     getE('messages').innerHTML = html;
+    }
 }
 
 
-
- 
-
-});
-
-$(function(){
-
-$('#btnReset').on('click', reset); 
-
-function reset() {
-    for (var i = 0; i < inputs.length; i++) {
-        var elm = getE(inputs[i])
-        elm.value = '';
-        elm.classList.remove(ERROR_CLASS);
+    function getE(id) {
+        return document.getElementById(id);
     }
 
-    getE('messages').innerHTML = '';
-
-       getE('terms').checked = false;
-}
-
- 
-function getE(id) {
-    return document.getElementById(id);
-}
+    function reset() {
+        for (var i = 0; i < inputs.length; i++) {
+            var elm = getE(inputs[i])
+            elm.value = '';
+            elm.classList.remove(ERROR_CLASS);
+        }
+        getE('messages').innerHTML = '';
+        getE('terms').checked = false;
+        getE('textarea').defaultValue = '';
+        getE('findUs').selected = '';
+    }
 });
+
+/* Challenge:  AJAX Post the form to the url '/AddEntry'.
+*/
+
+$("btnSubmit").click(function(){
+    var x = $("form").serializeArray();
+    $.each(x, function(i, field){
+        $("#results").append(field.name + ":" + field.value + " ");
+    });
+});
+
+
+myformobj = 
+$.post('/AddEntry', '$("#results")' , function(response){
+    console.log('failure')
+});
+  
